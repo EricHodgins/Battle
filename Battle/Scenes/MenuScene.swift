eda4e68c54f2f89679f613767e68166f1809a31a
Eric Hodgins <hodgins.e@gmail.com>
2019-02-15 21:40:21 -0500

    adjusted menu scene coords and add background.

--- Battle/Scenes/MenuScene.swift
+++ Battle/Scenes/MenuScene.swift
@@ -17,18 +17,21 @@ class MenuScene: SKScene {
     
     override func didMove(to view: SKView) {
         self.backgroundColor = UIColor.black
-        self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        self.anchorPoint = CGPoint(x: 0, y: 0)
+        
+        let backgroundTexture = Background()
+        backgroundTexture.spawn(parentNode: self, imageName: "background_main")
         
         let title = SKLabelNode(fontNamed: "Arial Rounded MT Bold")
         title.text = "BATTLE"
-        title.position = CGPoint(x: 0, y: 150)
+        title.position = CGPoint(x: size.width/2, y: size.height - 200)
         title.fontSize = 60
         self.addChild(title)
         
         let highScore = SKLabelNode(fontNamed: "Arial Rounded MT Bold")
         let highest = highestRound()
         highScore.text = "Record: \(highest)"
-        highScore.position = CGPoint(x: 0, y: title.position.y - 70)
+        highScore.position = CGPoint(x: size.width/2, y: title.position.y - 70)
         highScore.fontSize = 30
         self.addChild(highScore)
         
@@ -36,6 +39,7 @@ class MenuScene: SKScene {
         playButton.size = CGSize(width: 84, height: 49)
         playButton.name = "play"
         playButton.zPosition = 5
+        playButton.position = CGPoint(x: size.width/2, y: highScore.position.y - 90)
         self.addChild(playButton)
         
         viewController?.showAdBanner = true
@@ -58,7 +62,7 @@ class MenuScene: SKScene {
     
     private func highestRound() -> Int {
         let defaults = UserDefaults.standard
-        let round = defaults.integer(forKey: "Round") // returns 0 is key does not exist
+        let round = defaults.integer(forKey: "Round") // returns 0 if key does not exist
         return round
     }
  
